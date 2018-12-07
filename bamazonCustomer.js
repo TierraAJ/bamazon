02dda8e1845c53ff5afd2c7f0c9b86a622c64bdd
Tierra Austin-Johnson <38711594+TbabyAJ@users.noreply.github.com>
2018-12-07 07:22:24 -0500

    ensuring code updated

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,13 +1,15 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 require("console.table");
+
 var connection = mysql.createConnection({
-    database: "bamazon_db",
+   database: "bamazon_db",
    host: "localhost",
    port: "3306",
    user: "root",
    password: "nmIP**886758**"
 });
+
 connection.connect(function(err){
     if (err){
         console.log(err);
@@ -21,6 +23,7 @@ connection.connect(function(err){
 function shop() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err){
+            throw err,
             console.log(err);
         }
         else {
@@ -35,7 +38,10 @@ function customerOrder(inventory) {
         {
             type: "integer",
             name: "item-id",
-            message: "What is the item ID of the product you would like to purhase?"
+            message: "What is the item ID of the product you would like to purhase?",
+            validate: function(val) {
+                return !isNaN(val) || val.toLowerCase() === "q";
+              }
         },
         {
             type: "integer",
@@ -43,7 +49,21 @@ function customerOrder(inventory) {
             message: "How many would you like?"   
         }
     ])
-    .then(function(res){
-        console.log(res);
-    });
+    .then(function(val) {
+        // Check if the user wants to quit the program
+        checkIfShouldExit(val.choice);
+        var choiceId = parseInt(val.choice);
+        var product = checkInventory(choiceId, inventory);
+  
+        // If there is a product with the id the user chose, prompt the customer for a desired quantity
+        if (product) {
+          // Pass the chosen product to promptCustomerForQuantity
+          promptCustomerForQuantity(product);
+        }
+        else {
+          // Otherwise let them know the item is not in the inventory, re-run loadProducts
+          console.log("\nThat item is not in the inventory.");
+          loadProducts();
+        }
+      });
 }
